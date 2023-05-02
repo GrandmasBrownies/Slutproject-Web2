678c4b1e736d5e1fb80acf58978a43fb4305e472
GrandmasBrownies <92189822+GrandmasBrownies@users.noreply.github.com>
2023-05-02 16:14:36 +0200

    mycket ny skit, mestadels med posts

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ const upload = require('./upload');
 app.use(express.static(path.resolve('./public')));
 
 
-
-var obj = {};
-
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs');
 
 
-app.get('/', function(req,res){
+var groclist = {};
+
+
+let items = [];
+
+app.get('/grocery', function(req,res){
     res.render('grocery', { items: items });
     let sql = 'SELECT * FROM tabellnamn ORDER BY date DESC';
     db.query(sql, function(err, results){
@@ -24,35 +26,53 @@ app.get('/', function(req,res){
             throw err;
         } else {
             obj = {data: results};
-            console.log(obj)
-            res.render('home', obj)
+            console.log(groclist)
+            res.render('home', groclist);
         }
     });
 });
 
-let items = [];
 
 app.post('/add', (req, res) => {
     const newItem = req.body.item;
     items.push(newItem);
-    res.redirect('/');
+    res.redirect('/grocery');
   });
-  
-app.post('/remove', (req, res) => {
+
+ app.post('/remove', (req, res) => {
     const removeItem = req.body.item;
     items = items.filter(item => item !== removeItem);
-    res.redirect('/');
-  });
+    res.redirect('/grocery');
+ });
+    
+var obj = {};
 
 
-
-
-app.get('/home', function(req,res){
-    res.render('home')
+ app.get('/home', function(req,res){
+   let sql = 'SELECT * FROM tabellnamn ORDER BY date DESC';
+   db.query(sql, function(err, results) {
+        if(err) {
+            throw err;
+        } else {
+            obj = {data: results};
+            console.log(obj)
+            res.render('home', obj)
+        }
+   });
  });
 
-app.get('/share', function(req,res){
+ app.get('/postLayouts/default:id', function(req,res) {
+    const id = req.params.id;
+    res.render('default')
+ });
+
+
+ app.get('/share', function(req,res){
     res.render('share')
+ });
+
+ app.get('/post', function(req,res){
+    res.render('post')
  });
 
  app.get('/favourite', function(req,res){
@@ -74,17 +94,16 @@ app.get('/share', function(req,res){
  app.post('/post', upload.single('img'), function(req,res){
     const title = req.body.title;
     const text = req.body.text;
-    const img = "/uploads/" + req.file.filename;
+    const img = "/uploads/" + req.file.img;
     const sqlInstert = "INSERT INTO tabellnamn (text1, text2, text3) VALUES (?, ?, ?);"
     db.query(sqlInstert, [title, text, img], (err, result)=> {
         if(err) {
             throw err;
         } else {
-            res.render ('home');
+            res.redirect('home')
         }
     });
  });
- 
 
 
 app.listen(process.env.PORT || 3000, function(){
